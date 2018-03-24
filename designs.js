02c8ea774f8d8de0b0e6ad41b287bf7506a0f15f
Birat Rai <biratrai@hotmail.com>
2018-03-24 17:21:11 -0400

    first commit

--- designs.js
+++ designs.js
@@ -3,8 +3,35 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+$( "#sizePicker" ).submit(function( event ) {
+  var height = $('#inputHeight').val();
+  var weight = $('#inputWeight').val();
+  makeGrid(height, weight);
+  event.preventDefault();
+});
 
-// Your code goes here!
-
+function makeGrid(height, weight) {
+	$('#pixelCanvas').empty();
+	for(i = 0; i < height; i++){
+		str = '<tr>';
+		for (w = 0; w < weight; w++) {
+			str += '<td></td>';
+		}
+		str += '</tr>';	
+		$('#pixelCanvas').append( str );
+	}
+	
 }
+
+$('#pixelCanvas').on("click", " td", function(event) {
+	var x = $(this).css('backgroundColor');
+	console.log(x);
+	color = $('#colorPicker').val();
+	if(x === 'rgba(0, 0, 0, 0)'){
+		$(this).css({backgroundColor: color});	
+	} else {
+		$(this).css({backgroundColor: 'rgba(0, 0, 0, 0)'});	
+	}
+    
+});
+
